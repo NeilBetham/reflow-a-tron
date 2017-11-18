53ed998513802f0ad9dbe930d8f6f6cb7eab7c16
Neil Betham <nbetham@me.com>
2017-11-18 17:44:43 -0500

    Herp derp

--- controls/tc.c
+++ controls/tc.c
@@ -1,9 +1,9 @@
 #include "tc.h"
 
 void read_tc(){
+	uint8_t buf[2] = {0};
+	read_from_spi(2, &buf);
 	for(int i = 0; i < 2; i++){
-		uint8_t buf = 0;
-		read_from_spi(1, &buf);
-		current_temp.bytes[1 - i] = buf;
+		current_temp.bytes[1 - i] = buf[i];
 	}
 }
