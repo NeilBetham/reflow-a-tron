3226bad8a479f6b3cf969993077d4519970ad179
Neil Betham <nbetham@me.com>
2017-11-19 18:03:13 -0500

    Switching to C++, code moved to old for reference

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Reflow-A-Tron.cpp
+ *
+ * Created: 11/19/2017 6:02:19 PM
+ * Author : nbeth
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
