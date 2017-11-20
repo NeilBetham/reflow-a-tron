d290debf5d60f2c240b4fc6182a9b6e19c1e4637
Neil Betham <nbetham@me.com>
2017-11-20 02:25:40 -0500

    Remove build products

--- Debug/src/tc.d
+++
@@ -1,9 +0,0 @@
-src/tc.d src/tc.o: ../src/tc.cpp ../include/tc.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-../include/tc.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
